71dd4677ea7843f5b82584160310dae733dd6c87
Inlustra <tjnairn@gmail.com>
2016-02-26 18:21:09 +0000

    Update Main.java

--- src/com/thenairn/asynctest/Main.java
+++ src/com/thenairn/asynctest/Main.java
@@ -7,18 +7,23 @@ import java.util.Date;
 public class Main {
 
 
-
+    /**
+     * Application entry point
+     * Creates the consumer in a new thread in order to prevent blocking.
+     */
     public static void main(String[] args) {
-
         LogConsumer consumer = new LogConsumer();
         Thread thread = new Thread(consumer);
         thread.start();
-
         for(int i = 0; i < 1000; i++) { //Generate 1000 threads to supply the queue with lots of data
             startWaitThread(consumer, (long) (Math.random() * 10)+10);
         }
     }
 
+    /**
+     * A method used to spawn threads that will sleep for the 
+     * allotted amount of time, and then print the current date to the logger
+     */
     private static void startWaitThread(final LogConsumer consumer, final long waitTime) {
         new Thread(() -> {
             while (true) {
